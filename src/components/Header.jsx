22e84be23afa415fdff7e5f5e681e78f8d6f39d3
brutalkingran <swkvideogames@gmail.com>
2025-06-13 19:09:14 -0300

    header estilizado para mobiles, tailwind incorporado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,44 @@ import WatchlistModal from "./WatchlistModal";
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <div>
-      {/* LOGO */}
-      {/* MENU HAMBURGUESA */}
-      <button>
-        <AiOutlineMenu/>
-      </button>
-      {/* BOTON INICIO */}
-      <button>
-        <AiOutlineHome/>
-      </button>
-      {/* BOTON WATCHLIST */}
-      <button onClick={() => setIsModalOpen(!isModalOpen)}>
-        <AiOutlinePlaySquare />
-      </button>
-      {/* BOTON CONFIG */}
-      <button>
-        <AiOutlineSetting/>
-      </button>
+    <>
+      <div className="right-0 left-0 bottom-0 fixed bg-black py-1 opacity-90 border-t-1 inset-shadow-sm ">
+        <nav className="flex justify-around w-full">
+          {/* LOGO */}
+          {/* TODO: AÑADIR LOGO */}
+
+          {/* MENU HAMBURGUESA */}
+          <button className="hidden md:flex" onClick={() => setOpenMenu( !openMenu )}>
+            <AiOutlineMenu size={25}/>
+          </button>
+
+          {/* BOTON INICIO */}
+          <button className="mx-10 text-white text-sm/6 flex flex-col items-center">
+            <AiOutlineHome size={25}/>
+            <p>Home</p>
+          </button>
+
+          {/* BOTON WATCHLIST */}
+          <button className="mx-10 text-white text-sm/6 flex flex-col items-center" onClick={() => setIsModalOpen(!isModalOpen)}>
+            <AiOutlinePlaySquare size={25} />
+            <p>WatchList</p>
+          </button>
+
+          {/* BOTON CONFIG */}
+          <button className="mx-10 text-white text-sm/6 flex flex-col items-center">
+            <AiOutlineSetting size={25}/>
+            <p>Settings</p>
+          </button>
+        </nav>
+      </div>
 
       {
         isModalOpen && <WatchlistModal watchlist = { watchlist } onClose = {() => setIsModalOpen(false)} />
       }
-    </div>
+    </>
   )
 }
 
