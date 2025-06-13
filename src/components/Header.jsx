eb2996a38830dc32456247a3f6a76e57afb3d7b9
brutalkingran <swkvideogames@gmail.com>
2025-06-13 17:35:54 -0300

    header and modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { AiOutlinePlaySquare, AiOutlineSetting, AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+import WatchlistModal from "./WatchlistModal";
+
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>Header</div>
+    <div>
+      {/* LOGO */}
+      {/* MENU HAMBURGUESA */}
+      <button>
+        <AiOutlineMenu/>
+      </button>
+      {/* BOTON INICIO */}
+      <button>
+        <AiOutlineHome/>
+      </button>
+      {/* BOTON WATCHLIST */}
+      <button onClick={() => setIsModalOpen(!isModalOpen)}>
+        <AiOutlinePlaySquare />
+      </button>
+      {/* BOTON CONFIG */}
+      <button>
+        <AiOutlineSetting/>
+      </button>
+
+      {
+        isModalOpen && <WatchlistModal watchlist = { watchlist } onClose = {() => setIsModalOpen(false)} />
+      }
+    </div>
   )
 }
 
--- src/components/WatchlistModal.jsx
+++ src/components/WatchlistModal.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { AiOutlineDelete } from "react-icons/ai";
 
-const WatchlistModal = () => {
+const WatchlistModal = ({ watchList }) => {
+  const removeFromWatchList = ( id ) => {
+    const updatedList = watchlist.filter( movie => movie.id !== id);
+    localStorage.setItem("watchlist", JSON.stringify(updatedList));
+  }
+
   return (
-    <div>WatchlistModal</div>
+    <div>
+      <li>
+        { watchList.map(( movie ) => {
+          <ul key={ movie.id }>
+            <div>
+              <p> { movie.title } </p>
+            </div>
+            <div>
+              <button onClick={ () => removeFromWatchList( movie.id ) }>
+                <AiOutlineDelete/>
+              </button>
+            </div>
+          </ul>
+        })}
+      </li>
+    </div>
   )
 }
 
